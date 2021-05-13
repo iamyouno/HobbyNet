95bc04a7c1e1ad501dd3b78672a869c1bed2f630
younho117 <117jyh@gmail.com>
2021-05-13 20:58:12 +0900

    init with reviewWrite

--- src/reviewpage/reviewPage.js
+++ src/reviewpage/reviewPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ReviewWrite from './reviewWrite.js';
 
 function reviewPage(){
     return(
+        <div>
         <div>reviewPage</div>
+        <ReviewWrite/>
+        </div>
     )
 }
 
