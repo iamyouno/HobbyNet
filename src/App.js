bfbaf23dcf0e05acd33bbd97685c32e0cf97839c
gud0824 <77267900+gud0824@users.noreply.github.com>
2021-05-21 21:20:14 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Main from './main.js'
 import HobbyTest from './hobbytest/hobbyTest.js'
+import Test from './hobbytest/test.js'
 import Schedule from './schedule/schedule.js'
 import ReviewPage from './reviewpage/reviewPage.js'
 import { Route, BrowserRouter as Router } from 'react-router-dom';
@@ -10,6 +11,7 @@ function App() {
         <Router>
             <Route exact path='/'><Main/></Route>
             <Route path='/hobbyTest'><HobbyTest/></Route>
+            <Route path='/test'><Test/></Route>
             <Route path='/schedule'><Schedule/></Route>
             <Route path='/reviewPage'><ReviewPage/></Route>
         </Router>
