114669f6f1b06005cf9cd17a0c937bb6bbf1c90c
gud0824 <77267900+gud0824@users.noreply.github.com>
2021-05-23 01:52:36 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './main.css'
+import test from './test.png'
+import review from './search.png'
+import timetable from './schedule.png'
 
 function main(){
     return(
-        <div>
-        <div>main</div>
-            <Link to ='/hobbytest'><button style={{height: '30px', margin: '20px'}}>hobbytest</button></Link>
-            <Link to ='/schedule'><button style={{height: '30px', margin: '20px'}}>schedule</button></Link>
-            <Link to ='/reviewPage'><button style={{height: '30px', margin: '20px'}}>reviewPage</button></Link>
+        <div class = "main">
+            HobbyNet
+            <div class = "main_buttons">
+                <div class = "main_button test">
+                    <Link to ='/hobbytest' style={{textDecoration: 'none'}}>
+                        <img src={test} alt="test" height="25%" class = "image"/>
+                        <div class = "icon">
+                            <div class = "icon_text">Hobby test</div>
+                        </div>
+                    </Link>
+                </div>
+                <div class = "main_button review">
+                    <Link to ='/reviewPage' style={{textDecoration: 'none'}}>
+                        <img src={review} alt="test" height="25%" class = "image"/>
+                        <div class = "icon">
+                            <div class = "icon_text">Review</div>
+                        </div>
+                    </Link>
+                </div>
+                <div class = "main_button schedule">
+                    <Link to ='/schedule' style={{textDecoration: 'none'}}>
+                        <img src={timetable} alt="test" height="25%" class = "image"/>
+                        <div class = "icon">
+                            <div class = "icon_text">Timetable</div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
