1358d256b3b4d59b51f8d06e5a5565efe4dae54a
bara28 <bara28@kaist.ac.kr>
2021-05-23 14:46:57 +0900

    보연 1차

--- src/reviewpage/reviewPage.js
+++ src/reviewpage/reviewPage.js
@@ -5,6 +5,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as faHeartActive } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartInactive } from '@fortawesome/free-regular-svg-icons';
 import { faEdit } from '@fortawesome/free-regular-svg-icons';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import ReactStars from "react-rating-stars-component";
 
 function ReviewPage(){
     const [review, setReview] = useState({
@@ -17,7 +20,7 @@ function ReviewPage(){
         date: "0",
         active: -1
     })
-
+    
     const [viewContent, setViewContent] = useState([{
         id: 1,
         content: 'hi', 
@@ -60,6 +63,93 @@ function ReviewPage(){
         setViewContent(newArr)
     }
 
+/*여기서부터 return까지 보연씀 */
+var pointlist=[0,0,0];
+var comments="";
+
+
+const ratingChanged = (index)=>(newRating) => {
+  pointlist[index]=newRating;
+  let newreview = review;
+  newreview.total = pointlist[0];
+  newreview.interest = pointlist[1];
+  newreview.schedule = pointlist[2];
+  setReview(newreview)
+};
+const [hastag,setss]=useState({
+  items: [],
+  focused: false,
+  });
+const [Dayday, setdayday] = useState([{ day:"MON", yes: false, time:[0,0,0,0]},{day:'TUE',yes: false, time:[0,0,0,0] },
+  {day:"WED", yes: false,time:[0,0,0,0] },{day:'THU',yes: false, time:[0,0,0,0] },{day:'FRI', yes: false,time:[0,0,0,0] },{day:'SAT', yes: false,time:[0,0,0,0] },
+  {day:'SUN',yes: false, time:[0,0,0,0] }])
+
+const Submit = (evt)=>{
+console.log(review)
+}
+
+const handleInputChange=(evt)=> {
+let newVal = hastag
+newVal.input = evt.target.value
+setss(newVal);
+}
+
+const handleInputKeyDown=(evt)=> {
+if ( evt.keyCode === 13 ) {
+  const {value} = evt.target;
+  
+  let newreview = review;
+  newreview.hashtag = [...hastag.items,value]
+  setReview(newreview)
+
+  setss({
+    items: [...hastag.items, value],
+  });
+  evt.target.value="";
+
+}
+/*
+if ( hastag.items.length && evt.keyCode === 8 && !hastag.input.length ) {
+  setss(hastag => ({
+    items: hastag.items.slice(0, hastag.items.length - 1)
+  }));
+}*/
+}
+
+const handleRemoveItem =(index)=> {
+return () => {
+  let newreview = review;
+  newreview.hashtag = hastag.items.filter((item, i) => i !== index);
+  setReview(newreview)
+  setss(hastag => ({
+    items: hastag.items.filter((item, i) => i !== index)
+  }));
+
+}
+}
+
+
+const updateClick= index=>e=> {
+  let newArr = [...Dayday];
+  newArr[index].yes = !newArr[index].yes;
+  setdayday(newArr);
+}
+
+const updateTime= (index1,index2)=>e=> {
+let newArr = [...Dayday];
+newArr[index1].time[index2] = e.value;
+setdayday(newArr);
+}
+const updatecomment = e=>{
+  comments=e.target.value;
+  let newreview = review;
+  newreview.content = comments;
+  setReview(newreview)
+}
+
+
+
+
     return(
         <div>
             <div className="header">
@@ -99,8 +189,157 @@ function ReviewPage(){
                 )}
             
             </div>
-            <button className="button-write" onClick={()=>alert("ih")}><FontAwesomeIcon icon={faEdit} id="icon"/>write</button>
-            <ReviewWrite/>
+            <Popup trigger={<button className="button-write"><FontAwesomeIcon icon={faEdit} id="icon"/>write</button>} modal nested>
+            {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Share your hobby experience </div>
+        <div className="content">
+        <div className="bigwrapper"> 
+      <div className = "wrapper">    
+        <div className="full"> 
+        <h1>
+          HOBBY REVIEW
+        </h1>
+        <div>
+          <table>
+          <tbody>
+              <tr>
+    <td>Add categories:
+      <label>
+          <ul className="container">
+            {(hastag.items).map((item, i) => 
+              <li key={i} className="items" onClick={handleRemoveItem(i)}>
+                {item}
+                <span>(x)</span>
+              </li>
+            )}
+            <input
+              value={hastag.input}
+              onChange={handleInputChange}
+              onKeyDown={handleInputKeyDown} />
+          </ul>
+        </label>
+      </td>
+    </tr><tr>
+              <td>Comments:<textarea id="hashtagbox" onInput={updatecomment} cols="40" rows="3" charswidth="30" ></textarea></td>
+              </tr>
+              </tbody>
+              </table>
+        </div>
+ 
+        <div id="starbox">
+            <table>
+              <tbody>
+              <tr>
+                  <td>Total</td>
+                  <td>
+    <ReactStars
+      count={5}
+      onChange={ratingChanged(0)}
+      size={24}
+      isHalf={true}
+      emptyIcon={<i className="far fa-star"></i>}
+      halfIcon={<i className="fa fa-star-half-alt"></i>}
+      fullIcon={<i className="fa fa-star"></i>}
+      activeColor="#ffd700"
+    />
+            </td>
+              </tr>
+              <tr>
+                  <td>Interest</td>
+                  <td>    <ReactStars
+      count={5}
+      onChange={ratingChanged(1)}
+      size={24}
+      isHalf={true}
+      emptyIcon={<i className="far fa-star"></i>}
+      halfIcon={<i className="fa fa-star-half-alt"></i>}
+      fullIcon={<i className="fa fa-star"></i>}
+      activeColor="#ffd700"
+    /></td>
+              </tr>
+              <tr>
+                  <td>Schedule</td>
+                  <td><ReactStars
+      count={5}
+      onChange={ratingChanged(2)}
+      size={24}
+      isHalf={true}
+      emptyIcon={<i className="far fa-star"></i>}
+      halfIcon={<i className="fa fa-star-half-alt"></i>}
+      fullIcon={<i className="fa fa-star"></i>}
+      activeColor="#ffd700"
+    /></td>
+              </tr>
+              </tbody>
+            </table>
+        </div>
+        <div id ="hashtag box">
+            <table> 
+              <tbody>
+            {Dayday.map( (element, index) => <tr>
+            <td>{element.day}</td>
+            <td><button type="button" onClick={updateClick(index)} className={element.yes?"emptycirbtn":"fullcirbtn"}></button></td>
+            <td>No</td> 
+            <td><button onClick={updateClick(index)} className={element.yes ? "fullcirbtn":"emptycirbtn"}></button></td>
+            <td>Yes</td>  
+            <td>from  <input type ="number" onInput={updateTime(index,0)} id="hour" min={0} max={24} step={1} className={element.yes?"happy":"greyback"}/> :  
+            <input type ="number" onInput={updateTime(index,1)} min={0} max={50} step={10}  className={element.yes?"happy":"greyback"} /> 
+             to  <input type ="number" onInput={updateTime(index,2)} min={0} max={24} step={1}  className={element.yes?"happy":"greyback"}/> : 
+             <input type ="number" onInput={updateTime(index,3)} min={0} max={50} step={10}  className={element.yes?"happy":"greyback"}/>                
+             </td>
+                </tr>)}
+                </tbody>
+            </table>
+        </div>
+      </div>
+      </div>
+      </div>
+        </div>
+        <div className="actions">
+        <button
+            className="button"
+            onClick={() => {
+                setReview({
+                    content: '',
+                    like: 0,
+                    total: parseFloat(0),
+                    interest: 0,
+                    schedule: 0,
+                    hashtag: [],
+                    date: "0",
+                    active: -1
+                })
+                setdayday([{ day:"MON", yes: false, time:[0,0,0,0]},{day:'TUE',yes: false, time:[0,0,0,0] },
+  {day:"WED", yes: false,time:[0,0,0,0] },{day:'THU',yes: false, time:[0,0,0,0] },{day:'FRI', yes: false,time:[0,0,0,0] },{day:'SAT', yes: false,time:[0,0,0,0] },
+  {day:'SUN',yes: false, time:[0,0,0,0] }])
+                setss({
+                    items: [],
+                    focused: false,
+                    })
+              console.log('Cancel');
+              close();
+            }}
+          >
+            close
+          </button>
+          <button
+            className="button"
+            onClick={() =>{
+              Submit()
+              console.log('Accept');
+              close();
+            }}
+          >
+            Accept
+          </button>
+        </div>
+      </div>
+    )}
+  </Popup>
         </div>
     )
 }
